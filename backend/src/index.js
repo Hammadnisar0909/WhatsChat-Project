0e7190c5079b10ec522cd0e17ca12ad933e3707a
Hammadnisar0909 <hammadnisar0909@gmail.com>
2024-12-25 12:53:08 +0500

    6th update

--- backend/src/index.js
+++ backend/src/index.js
@@ -56,6 +56,14 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' blob:;"
+  );
+  next();
+});
+
 // Start the server
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
